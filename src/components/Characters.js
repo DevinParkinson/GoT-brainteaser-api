0f7fcf95d87f111295976b2b5b6a89d2cb757388
Devin Parkinson <dparky13@icloud.com>
2018-04-13 11:01:04 -0600

    working

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,15 +1,48 @@
 import React from 'react';
+import { Container, Header, Card, } from 'semantic-ui-react';
+import {connect} from 'react-redux';
 import axios from 'axios';
 
 class Characters extends React.Component {
+  state = { characters: []}
 
   componentDidMount() {
-    const { dispatch } = this.props;
-    axios.get('https://api.got.show/api/characters')
+    axios.get('https://api.got.show/api/characters/locations')
       .then( res => {
+        this.setState({ characters: res.data })
         console.log(res.data)
+      }).catch(err => {
+        console.log(err);
       })
-  }.catch(err => {
-    console.log(err);
-  })
+  }
+
+  displayCharacters = () => {
+    const {characters} = this.state;
+    return characters.map(c => {
+      return (
+        <Card key={c.name}>
+          <Card.Content>
+            <Card.Header>{c.name}</Card.Header>
+          </Card.Content>
+          <Card.Content>
+          {c.locations}
+          </Card.Content>
+        </Card>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <Container>
+        <Header textAlign="center">List of Characters in GOT</Header>
+        <Card.Group>
+        {this.displayCharacters()}
+        </Card.Group>
+      </Container>
+    )
+  }
+
 }
+
+export default connect()(Characters);
